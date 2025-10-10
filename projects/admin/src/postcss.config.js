f4620037741fbde056bdb6beb9ebbde873b112b9
osama <osamaromih2020@gmail.com>
2025-10-10 06:03:48 +0200

    before angular update

---
+++ projects/admin/src/postcss.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./projects/admin/src/**/*.{html,ts}",
+    "./projects/patient/src/**/*.{html,ts}",
+    "./projects/secretary/src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
